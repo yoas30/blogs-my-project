f678de4327b8138c3e03e48dbbcf17c6afd6b004
yoas30 <yoas.rinatan30@gmail.com>
2025-03-04 15:04:40 +0700

    Route Handling method GET Detail ke MongoDB

--- src/app/api/blogs/[id]/route.ts
+++ src/app/api/blogs/[id]/route.ts
@@ -1,7 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/database";
+import { mongo_uri } from "../../../../../constant";
+import { Blog } from "@/models/blog";
 
 export async function GET (req:NextRequest, {params}: {params: {id:string}}){
-    return new NextResponse (`Get Blog Detail ${params.id}`)
+    try {
+        await connectDB(mongo_uri)
+        const blog = await Blog.findById(params.id)
+        return NextResponse.json({
+            success : true,
+            data : blog
+        })
+    } catch (error:any) {
+        if (error.name === 'CatchError' && error.kind === 'ObjectId'){
+            return NextResponse.json({
+                success : false,
+                message : 'resource error'
+            },{
+                status: 404,
+            })
+        }
+        
+        return NextResponse.json({
+            success : false,
+            message : error.message
+        },{
+            status: 500,
+        })
+    }
 }
 
 export async function DELETE (req:NextRequest, {params}: {params: {id:string}}){
