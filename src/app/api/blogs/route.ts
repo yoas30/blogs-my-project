c8d0ec49a641677282884ba1f794ac65b1698819
yoas30 <yoas.rinatan30@gmail.com>
2025-03-04 14:48:28 +0700

    Route Handling method GET ke MongoDB

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -3,10 +3,35 @@ import connectDB from "@/database";
 import { mongo_uri } from "../../../../constant";
 import { Blog } from "@/models/blog";
 
+////////////////////////////////////////////////-- Fungsi GET --////////////////////////////////////////////////
+
 export async function GET (req:NextRequest){
-    await connectDB (mongo_uri)
-    return new NextResponse ("Get Blogs")
+    const searchParamms = req.nextUrl.searchParams
+
+    //get 10 data dengan min 1 page
+    const limit = searchParamms.get('limit') || '10'
+    const page = searchParamms.get('page') || '1'
+    const skip  =  (parseInt(page) - 1 ) * parseInt(limit)
+
+    try {
+        await connectDB (mongo_uri)
+        const blogs = await Blog.find().sort({date:-1}).skip(skip).limit(parseInt(limit))
+        const totalData = await Blog.countDocuments()
+        return NextResponse.json({
+            total: totalData,
+            page: page,
+            limit: limit,
+            data: blogs
+        })
+    } catch (error:any) {
+        return NextResponse.json({
+            success : false,
+            message : error.message
+        },{status: 500})
+    }
 }
+
+////////////////////////////////////////////////-- Fungsi POST --////////////////////////////////////////////////
 
 export async function POST (req:NextRequest){
     try {
